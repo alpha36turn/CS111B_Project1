9302c17003c4709e295a20195da1cf06153643bf
alpha36turn <alpha36turn@gmail.com>
2016-08-28 13:23:13 -0700

    Update Play.java

--- src/main/java/com/mycompany/raffle/Play.java
+++ src/main/java/com/mycompany/raffle/Play.java
@@ -5,8 +5,25 @@ public class Play
 {
     public static void main(String[] args)
     {
+        Scanner scan = new Scanner(System.in);
+        char keepGoing = 'y';
         Raffle r = new Raffle();
-        r.draw();
-        r.simulate();
+       
+            r.draw();
+            r.simulate();
+            
+        while (keepGoing != 'n') {
+            System.out.println("\nDo you want to keep playing? (y/n)"); 
+            keepGoing = scan.nextLine().charAt(0);
+			
+            if(keepGoing == 'n') {
+				System.out.println("Goodbye!");
+				}	
+            else  {
+               r.draw();
+               r.simulate();
+            }
+            	
+       	}
     }
 }
